7f6b8b472beedf1af6a9b2e036982adbcef6179b
Anastasiya <forquest@icloud.com>
2025-05-07 16:53:42 +0300

    спрятала креды в окружение, добавила его в гитигнор, вынесла все это в фикстуру

--- arda_tests/conftest.py
+++ arda_tests/conftest.py
@@ -1,14 +1,27 @@
-# import pytest
-#
-# @pytest.fixture(scope="session")
-# def base_url():
-#     return "https://it.arda.digital"
-
 
 import pytest
 import logging
 import allure
 from allure_commons.types import AttachmentType
+from dotenv import load_dotenv
+import os
+
+# Загружаем .env
+load_dotenv()
+
+@pytest.fixture(scope="session")
+def credentials():
+    identifier = os.getenv("API_IDENTIFIER")
+    password = os.getenv("API_PASSWORD")
+
+    if not identifier or not password:
+        raise EnvironmentError("Не заданы переменные API_IDENTIFIER или API_PASSWORD в .env")
+
+    return {
+        "identifier": identifier,
+        "password": password
+    }
+
 
 # Настройка логирования
 logger = logging.getLogger()
