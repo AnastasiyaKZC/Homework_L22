8bfb3f6e41148528deed95cbe479ec54c767b8f7
Anastasiya <forquest@icloud.com>
2025-05-21 10:25:31 +0300

    добавила тег и лейбл для отчета

--- arda_tests/test_api.py
+++ arda_tests/test_api.py
@@ -1,5 +1,90 @@
-import os
+# import os
+#
+# import requests
+# import allure
+# from conftest import log_request_and_response
+#
+#
+# @allure.epic("API тесты")
+# @allure.feature("Доступность страницы")
+# @allure.story("GET-запрос на / возвращает 200 OK")
+# @allure.severity(allure.severity_level.CRITICAL)
+# def test_main_page_is_available(base_url):
+#     url = f"{base_url}/"
+#
+#     with allure.step("Отправка GET-запроса на главный endpoint"):
+#         response = requests.get(url)
+#         log_request_and_response(response)
+#
+#     assert response.status_code == 200
+#
+#
+# @allure.epic("API тесты")
+# @allure.feature("Некорректные маршруты")
+# @allure.story("GET-запрос на несуществующий URL возвращает 404")
+# @allure.severity(allure.severity_level.NORMAL)
+# def test_page_not_found(base_url):
+#     url = f"{base_url}/blabla"
+#
+#     with allure.step("Отправка GET-запроса на несуществующий endpoint"):
+#         response = requests.get(url)
+#         log_request_and_response(response)
+#
+#     assert response.status_code == 404
+#
+#
+# @allure.epic("API тесты")
+# @allure.feature("Аутентификация")
+# @allure.story("POST-запрос с неверным паролем возвращает 400 Bad Request")
+# @allure.severity(allure.severity_level.CRITICAL)
+# def test_login_with_invalid_password(base_url, credentials):
+#     url = f"{base_url}/api/users/verify"
+#     payload = {
+#         "identifier": credentials["identifier"],
+#         "password": "wrong_password"  # специально неверный пароль
+#     }
+#
+#     with allure.step("Отправка POST-запроса с неверным паролем"):
+#         response = requests.post(url, json=payload)
+#         log_request_and_response(response)
+#
+#     assert response.status_code == 400
+#
+#
+# @allure.epic("API тесты")
+# @allure.feature("Аутентификация")
+# @allure.story("POST-запрос с верными кредами возвращает 200 ОК")
+# @allure.severity(allure.severity_level.CRITICAL)
+# def test_successful_login(base_url, credentials):
+#     url = f"{base_url}/api/users/verify"
+#     payload = {
+#         "identifier": credentials["identifier"],
+#         "password": credentials["password"]
+#     }
+#
+#     with allure.step("Отправка POST-запроса для авторизации с валидными данными"):
+#         response = requests.post(url, json=payload)
+#         log_request_and_response(response)
+#
+#     assert response.status_code == 200
+#
+#
+#
+# @allure.epic("API тесты")
+# @allure.feature("Редиректы")
+# @allure.story("GET-запрос на http:// редиректит на https://")
+# @allure.severity(allure.severity_level.NORMAL)
+# def test_http_redirect_to_https():
+#     url = "http://it.arda.digital"
+#
+#     with allure.step("Отправка GET-запроса по HTTP без следования редиректу"):
+#         response = requests.get(url, allow_redirects=False)
+#         log_request_and_response(response)
+#
+#     assert response.status_code in [301, 302], "Ожидается редирект"
+#     assert response.headers.get("Location", "").startswith("https://"), "Ожидается редирект на HTTPS"
 
+import os
 import requests
 import allure
 from conftest import log_request_and_response
@@ -9,6 +94,8 @@ from conftest import log_request_and_response
 @allure.feature("Доступность страницы")
 @allure.story("GET-запрос на / возвращает 200 OK")
 @allure.severity(allure.severity_level.CRITICAL)
+@allure.tag("api", "availability")
+@allure.label("owner", "Kuznetsova")
 def test_main_page_is_available(base_url):
     url = f"{base_url}/"
 
@@ -23,6 +110,8 @@ def test_main_page_is_available(base_url):
 @allure.feature("Некорректные маршруты")
 @allure.story("GET-запрос на несуществующий URL возвращает 404")
 @allure.severity(allure.severity_level.NORMAL)
+@allure.tag("api", "routing")
+@allure.label("owner", "Kuznetsova")
 def test_page_not_found(base_url):
     url = f"{base_url}/blabla"
 
@@ -37,6 +126,8 @@ def test_page_not_found(base_url):
 @allure.feature("Аутентификация")
 @allure.story("POST-запрос с неверным паролем возвращает 400 Bad Request")
 @allure.severity(allure.severity_level.CRITICAL)
+@allure.tag("api", "auth")
+@allure.label("owner", "Kuznetsova")
 def test_login_with_invalid_password(base_url, credentials):
     url = f"{base_url}/api/users/verify"
     payload = {
@@ -55,6 +146,8 @@ def test_login_with_invalid_password(base_url, credentials):
 @allure.feature("Аутентификация")
 @allure.story("POST-запрос с верными кредами возвращает 200 ОК")
 @allure.severity(allure.severity_level.CRITICAL)
+@allure.tag("api", "auth")
+@allure.label("owner", "Kuznetsova")
 def test_successful_login(base_url, credentials):
     url = f"{base_url}/api/users/verify"
     payload = {
@@ -69,11 +162,12 @@ def test_successful_login(base_url, credentials):
     assert response.status_code == 200
 
 
-
 @allure.epic("API тесты")
 @allure.feature("Редиректы")
 @allure.story("GET-запрос на http:// редиректит на https://")
 @allure.severity(allure.severity_level.NORMAL)
+@allure.tag("api", "redirect")
+@allure.label("owner", "Kuznetsova")
 def test_http_redirect_to_https():
     url = "http://it.arda.digital"
 
--- arda_tests/test_ui.py
+++ arda_tests/test_ui.py
@@ -2,23 +2,25 @@ from selene import have, be
 import allure
 from pages.auth_page import AuthPage
 from pages.main_page import MainPage
-from selene import be
 
 @allure.epic("UI тесты")
 @allure.feature("Лендинг")
 @allure.story("Логотип должен отображаться в хедере")
 @allure.severity(allure.severity_level.NORMAL)
+@allure.tag("landing", "header")
+@allure.label("owner", "Kuznetsova")
 def test_logo_is_visible(setup_browser):
     with allure.step("Открываем главную страницу"):
         setup_browser.open("https://it.arda.digital")
     with allure.step("Проверяем наличие изображения с логотипом в хедере"):
         setup_browser.element("header img").should(have.attribute("src"))
 
-
 @allure.epic("UI тесты")
 @allure.feature("Лендинг")
 @allure.story("Форма регистрации должна открываться по клику на кнопку")
 @allure.severity(allure.severity_level.CRITICAL)
+@allure.tag("landing", "registration")
+@allure.label("owner", "Kuznetsova")
 def test_registration_form_opens(setup_browser):
     with allure.step("Открываем главную страницу"):
         setup_browser.open("https://it.arda.digital")
@@ -29,11 +31,12 @@ def test_registration_form_opens(setup_browser):
     with allure.step("Проверяем, что форма регистрации отображается"):
         setup_browser.element("form").should(be.visible)
 
-
 @allure.epic("UI тесты")
 @allure.feature("Лендинг")
 @allure.story("Переход по ссылке arda.digital")
 @allure.severity(allure.severity_level.NORMAL)
+@allure.tag("landing", "navigation")
+@allure.label("owner", "Kuznetsova")
 def test_learn_more_link(setup_browser):
     with allure.step("Открываем главную страницу"):
         setup_browser.open("https://it.arda.digital")
@@ -49,53 +52,57 @@ def test_learn_more_link(setup_browser):
 @allure.feature("Форма авторизации")
 @allure.story("Проверка отправки пустой формы")
 @allure.severity(allure.severity_level.CRITICAL)
+@allure.tag("auth", "validation")
+@allure.label("owner", "Kuznetsova")
 def test_empty_auth_form(setup_browser):
-    auth_page = AuthPage(setup_browser)  # ✅ Передаем browser в AuthPage
+    auth_page = AuthPage(setup_browser)
     (
         auth_page.open()
         .open_auth_form()
         .submit()
     )
-    # Проверяем, что отображаются ошибки ввода
-    setup_browser.element("span.text-red").should(be.visible)  # ✅ Используем setup_browser
+    setup_browser.element("span.text-red").should(be.visible)
 
 @allure.epic("UI тесты")
 @allure.feature("Форма авторизации")
 @allure.story("Пользователь вводит неверный логин")
 @allure.severity(allure.severity_level.NORMAL)
+@allure.tag("auth", "negative")
+@allure.label("owner", "Kuznetsova")
 def test_invalid_login(setup_browser):
     auth_page = AuthPage(setup_browser)
     (
         auth_page.open()
         .open_auth_form()
-        .enter_email("wrong@arda.digital")  # ❌ Неверный логин
-        .enter_password("111111")  # ✅ Верный пароль
+        .enter_email("wrong@arda.digital")
+        .enter_password("111111")
         .submit()
     )
-    # Проверяем, что отображается ошибка авторизации
     setup_browser.element(".text-red").should(be.visible)
 
 @allure.epic("UI тесты")
 @allure.feature("Форма авторизации")
 @allure.story("Пользователь вводит неверный пароль")
 @allure.severity(allure.severity_level.NORMAL)
+@allure.tag("auth", "negative")
+@allure.label("owner", "Kuznetsova")
 def test_invalid_password(setup_browser):
     auth_page = AuthPage(setup_browser)
     (
         auth_page.open()
         .open_auth_form()
-        .enter_email("member@arda.digital")  # ✅ Верный логин
-        .enter_password("wrongpassword")  # ❌ Неверный пароль
+        .enter_email("member@arda.digital")
+        .enter_password("wrongpassword")
         .submit()
     )
-    # Проверяем, что отображается ошибка авторизации
     setup_browser.element(".text-red").should(be.visible)
-
 
 @allure.epic("UI тесты")
 @allure.feature("Форма авторизации")
 @allure.story("Пользователь успешно авторизуется")
 @allure.severity(allure.severity_level.CRITICAL)
+@allure.tag("auth", "positive")
+@allure.label("owner", "Kuznetsova")
 def test_successful_authorization(setup_browser, credentials):
     auth_page = AuthPage(setup_browser)
     main_page = MainPage(setup_browser)
